75d83ba16f894422f873ee8def47ac5f3902b539
kuoted <cvsuser@163.com>
2022-05-24 18:03:01 +0800

    update command.sh and install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -sf ~/MyConfiguration_OnGitee/MyDotFiles/vimrc ~/.vimrc
 ln -sf ~/MyConfiguration_OnGitee/MyDotFiles/config.clang-format ~/.clang-format 
 ln -sf ~/MyConfiguration_OnGitee/MyDotFiles/dot_vim ~/.vim
+ln -sf ~/MyConfiguration_OnGitee/MyDotFiles/command.sh ~/10_command.sh
